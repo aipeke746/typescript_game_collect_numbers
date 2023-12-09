f6423782a961c326414ad9946e8ea8dc68d83323
nakatsu746 <jilptfes746@gmail.com>
2023-12-09 18:22:07 +0900

    スワイプで移動できるように機能を追加

--- src/service/operate/direction/impl/manualImpl.ts
+++ src/service/operate/direction/impl/manualImpl.ts
@@ -9,6 +9,18 @@ export class ManualImpl implements OperateDirectionService {
      * キー入力
      */
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    /**
+     * ポインター
+     */
+    private pointer: Phaser.Input.Pointer;
+    /**
+     * スワイプ入力用： ポインターがアップされたことを記録するフラグ
+     */
+    private isPointerUp: boolean = false;
+    /**
+     * スワイプ入力用： キャラクターが移動可能かどうかを記録するフラグ
+     */
+    private canMove: boolean = true;
 
     /**
      * コンストラクタ
@@ -17,6 +29,11 @@ export class ManualImpl implements OperateDirectionService {
     constructor(scene: Phaser.Scene) {
         if (scene.input.keyboard == null) throw new Error('keyboard is null');
         this.cursors = scene.input.keyboard.createCursorKeys();
+        this.pointer = scene.input.activePointer;
+
+        // 酢ワイプ用のイベントハンドラを登録する
+        scene.input.on('pointerup', this.onPointerUp, this);
+        scene.input.on('pointerdown', this.onPointerDown, this);
     }
 
     /**
@@ -28,11 +45,24 @@ export class ManualImpl implements OperateDirectionService {
     }
 
     /**
-     * 方向キーの入力からキャラクターの移動方向を返す
+     * 方向キーとスワイプの入力のいずれかからキャラクターの移動方向を返す
      * @returns キャラクターの移動方向
      */
     private getManualDirection(): DirectionType {
+        const keyDirection = this.getKeyDirection();
+        const swipeDirection = this.getSwipeDirection();
+        return keyDirection !== DirectionType.NONE
+            ? keyDirection
+            : swipeDirection;
+    }
+
+    /**
+     * 方向キーの入力からキャラクターの移動方向を返す
+     * @returns キャラクターの移動方向
+     */
+    private getKeyDirection(): DirectionType {
         let direction = DirectionType.NONE;
+
         if (this.cursors.right.isDown) {
             direction = DirectionType.RIGHT;
         } else if (this.cursors.left.isDown) {
@@ -44,5 +74,59 @@ export class ManualImpl implements OperateDirectionService {
         }
 
         return direction;
+    }
+
+    /**
+     * スワイプの入力からキャラクターの移動方向を返す
+     * タッチした後に離した場合のみスワイプと判定する
+     * @returns キャラクターの移動方向
+     */
+    private getSwipeDirection(): DirectionType {
+        if (this.canMove && this.isPointerUp) {
+            return this.onSwipe();
+        }
+        return DirectionType.NONE;
+    }
+
+    /**
+     * スワイプの入力から移動方向を返す
+     * タッチした位置から離した位置までの距離と時間から移動方向を判定する
+     * @returns キャラクターの移動方向
+     */
+    private onSwipe(): DirectionType {
+        const swipeTime = this.pointer.upTime - this.pointer.downTime;
+        const swipe = new Phaser.Geom.Point(this.pointer.upX - this.pointer.downX, this.pointer.upY - this.pointer.downY);
+        const swipeMagnitude = Phaser.Geom.Point.GetMagnitude(swipe);
+        const swipeNormal = new Phaser.Geom.Point(swipe.x / swipeMagnitude, swipe.y / swipeMagnitude);
+        const swipeNormalAbsolute = new Phaser.Geom.Point(Math.abs(swipeNormal.x), Math.abs(swipeNormal.y));
+
+        if (swipeTime > 1000 || swipeMagnitude < 20)
+            return DirectionType.NONE;
+
+        this.canMove = false;
+        if (swipeNormalAbsolute.x > swipeNormalAbsolute.y) {
+            return swipeNormal.x > 0
+                ? DirectionType.RIGHT
+                : DirectionType.LEFT;
+        } else {
+            return swipeNormal.y > 0
+                ? DirectionType.DOWN
+                : DirectionType.UP;
+        }
+    }
+
+    /**
+     * ポインターのアップイベントハンドラ
+     */
+    private onPointerUp(): void {
+        this.isPointerUp = true;
+    }
+
+    /**
+     * ポインターのダウンイベントハンドラ
+     */
+    private onPointerDown(): void {
+        this.isPointerUp = false;
+        this.canMove = true;
     }
 }
